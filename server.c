b3474cc0288f2caed5239598aeeafccac52693b6
Segfault <charles.verchere-garcia-de-mateos@epitech.eu>
2020-04-16 09:28:14 -0700

    server coding style

--- server.c
+++ server.c
@@ -23,6 +23,45 @@ char *pwd(void)
     return (result);
 }
 
+void quit(int sd, struct sockaddr_in address, int addrlen)
+{
+    getpeername(sd, (struct sockaddr*)&address,
+                (socklen_t*)&addrlen);
+     printf("Host disconnected , ip %s , port %d\n",
+            inet_ntoa(address.sin_addr),
+            ntohs(address.sin_port));
+    close(sd);
+}
+
+char *password(int sd, char *buffer, char *rts)
+{
+    char *string = malloc(sizeof(char) * 80);
+
+    strcpy(string, buffer);
+    if (strncmp(rts, "USER Anonymous", 14) == 0)
+        write(sd, "230 User logged in, proceed\n", 28);
+    else
+        write(sd, "530 Not logged in\n", 18);
+    return (string);
+}
+
+char *username(int sd, char *buffer)
+{
+    char *rts = malloc(sizeof(char) * 80);
+    
+    strcpy(rts, buffer);
+    write(sd, "331 User name okay, need password\n", 34);
+    return (rts);
+}
+
+char *parseuser(char *buff)
+{
+    char *token = strtok(buff, "A");
+    while (token != NULL)
+        token = strtok(NULL, "A");
+    return (buff);
+}
+
 void strip_extra_spaces(char* str)
 {
   int i, x;
@@ -42,14 +81,6 @@ char *parsecmd(char *buff)
     return (buff);
 }
 
-char *parseuser(char *buff)
-{
-    char *token = strtok(buff, "A");
-    while (token != NULL)
-        token = strtok(NULL, "A");
-    return (buff);
-}
-
 int main(int ac, char **av)
 {
     int master_socket , addrlen , new_socket , client_socket[30],
@@ -59,10 +90,8 @@ int main(int ac, char **av)
     char *rts = malloc(sizeof(char) * 80);
     char *string = malloc(sizeof(char) * 80);
     char *user = malloc(sizeof(char) * 80);
-    // const char *str;
-    char buffer[1025];  //data buffer of 1K
+    char buffer[1025];
     fd_set readfds;
-    // bool pas = false;
 
     if (ac > 3 || ac < 3)
         return (84);
@@ -145,43 +174,28 @@ int main(int ac, char **av)
             sd = client_socket[i];
             if (FD_ISSET(sd , &readfds)) {
                 if ((valread = read(sd , buffer, sizeof(buffer))) <= 0) {
-                    getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-                    printf("Host disconnected , ip %s , port %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-                    close(sd);
+                    quit(sd, address, addrlen);
                     client_socket[i] = 0;
-                }
-                else {
+                } else {
                     parsecmd(buffer);
                     printf("%s\n", buffer);
                     strcpy(user, buffer);
                     parseuser(user);
                     // printf("%s\n", user);
-                    if (strncmp(buffer, "USER Anonymous", 14) == 0 || strncmp(user, "USER ", 4) == 0) {
-                        strcpy(rts, buffer);
-                        write(sd, "331 User name okay, need password\n", 34);
-                    }
-                    else if (strncmp(buffer, "PASS ", 5) == 0) {
-                        strcpy(string, buffer);
-                        if (strncmp(rts, "USER Anonymous", 14) == 0) {
-                            write(sd, "230 User logged in, proceed\n", 28);
-                            // pas = true;
-                        }
-                        else
-                            write(sd, "530 Not logged in\n", 18);
-                    }
+                    if (strncmp(buffer, "USER Anonymous", 14) == 0
+                        || strncmp(user, "USER ", 4) == 0)
+                        rts = username(sd, buffer);
+                    else if (strncmp(buffer, "PASS ", 5) == 0)
+                        string = password(sd, buffer, rts);
                     else if (strncmp(buffer, "QUIT", 4) == 0) {
-                        getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-                        printf("Host disconnected , ip %s , port %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-                        close(sd);
+                        quit(sd, address, addrlen);
                         client_socket[i] = 0;
                         break;
                     }
                     else if (strncmp(rts, "USER Anonymous", 14) == 0) {
                         if (strncmp(string, "PASS ", 5) == 0) {
                             if (strncmp(buffer, "QUIT", 4) == 0) {
-                                getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-                                printf("Host disconnected , ip %s , port %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-                                close(sd);
+                                quit(sd, address, addrlen);
                                 client_socket[i] = 0;
                                 break;
                             }
@@ -194,11 +208,8 @@ int main(int ac, char **av)
                                  write(sd, "214 Help message\n", 17);
                                  write(sd, "HELP NOOP PWD QUIT USER\n", 24);
                             }
-                            // else
-                            // write(sd, "500 Syntax error, command unrecognized\n", 39);
                         }
-                    }
-                    else
+                    } else
                         write(sd, "500 Syntax error, command unrecognized\n", 39);
                     bzero(buffer, sizeof(buffer));
                     FD_CLR(sd, &readfds);
